9ff76f9476593394e15aa2088d03ada7fc3ba393
Madhuri Angel Baxla <52277459+madhuriangel@users.noreply.github.com>
2025-07-17 09:28:33 +0100

    Add files via upload

---
+++ Darmaraki_method/allyears_mhw_barplot.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the CSV again (the full event list for both strongest and longest)
+df_all = pd.read_csv("mhw_results/all_mhw_events_by_grid.csv")
+
+# Group all MHW events by year
+df_all['Start Date'] = pd.to_datetime(df_all['Start Date'])
+df_all['Year'] = df_all['Start Date'].dt.year
+
+# Count all events per year
+all_events_per_year = df_all['Year'].value_counts().sort_index()
+
+# Plot all events per year
+# plt.figure(figsize=(14, 6))
+# plt.bar(all_events_per_year.index, all_events_per_year.values, color='teal')
+# plt.xlabel("Year")
+# plt.ylabel("Number of MHW Events")
+# plt.title("Yearly Count of All Marine Heatwave Events (Alter Method)")
+# plt.xticks(rotation=45)
+# plt.grid(axis='y')
+# plt.tight_layout()
+# plt.show()
+
+# Plot again with all years shown on x-axis
+plt.figure(figsize=(14, 6))
+plt.bar(all_events_per_year.index, all_events_per_year.values, color="#005E63")
+plt.xlabel("Year")
+plt.ylabel("Number of MHW Events")
+plt.title("Yearly Count of All Marine Heatwave Events (Darmaraki)")
+plt.xticks(ticks=all_events_per_year.index, rotation=45)
+plt.grid(axis='y')
+plt.tight_layout()
+plt.show()
